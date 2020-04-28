7672ed047e6f67ad0e33d0e1163afbc150870855
The-Code-Monkey <31218527+The-Code-Monkey@users.noreply.github.com>
2020-04-28 13:23:16 +0100

    Create styled.ts

---
+++ frontend/packages/atoms/box/styled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+import { compose, layout, space, color } from "styled-system";
+
+const StyledBox = styled.div`
+  display: block;
+  box-sizing: border-box;
+  ${compose(
+    layout,
+    space,
+    color
+  )}
+`;
