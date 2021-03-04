6fe9f4d6c585aecb906b023e753a45a7d91ebbcc
The-Code-Monkey <andy.wilson1234@gmail.com>
2021-03-04 10:58:47 +0000

    feat: rebuild website core

---
+++ frontend/styles/styled.ts
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+    html, body {
+        width: 100vw;
+        height: 100%;
+        margin: 0;
+        padding: 0;
+    }
+`;
